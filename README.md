3bece127fbdf479ce916307f996889c685fd028f
Thenvir <thenvira@gmail.com>
2022-02-02 01:53:07 -0500

    Add sliding window dir & first question

---
+++ README.md
@@ -0,0 +1,2 @@
+# Leet-Coding-Questions
+A collection of coding questions I've practiced with.
---
+++ Sliding-Window/219. Contains Duplicate II.js
@@ -0,0 +1,16 @@
+/* 
+    Given an integer array nums and an integer k, 
+    return true if there are two distinct indices i and j in the array 
+    such that nums[i] == nums[j] and abs(i - j) <= k. 
+ 
+ 
+    Input: nums = [1,2,3,1], k = 3
+    Output: true
+
+    Input: nums = [1,0,1,1], k = 1
+    Output: true
+
+    Input: nums = [1,2,3,1,2,3], k = 2
+    Output: false
+
+ */
